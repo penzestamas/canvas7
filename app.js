a910a2661784666d34f06861569b22b46a41199d
pthomas2001 <71702126+pthomas2001@users.noreply.github.com>
2021-03-10 12:44:44 +0100

    js

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
+
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
+var halfOfCanvasWidth = canvasWidth / 2;
+var halfOfCanvasHeight = canvasHeight / 2;
+
+context.beginPath();
+context.moveTo(halfOfCanvasWidth, canvasHeight - canvasHeight);
+context.lineTo(halfOfCanvasWidth, canvasHeight);
+context.strokeStyle = 'green';
+context.stroke();
+
+context.beginPath();
+context.moveTo(canvasWidth - canvasWidth, halfOfCanvasHeight);
+context.lineTo(canvasWidth, halfOfCanvasHeight);
+context.strokeStyle = 'red';
+context.stroke();
